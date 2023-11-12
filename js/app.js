269e96289349b06de91b8c83521ac72f67f51daa
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-12 18:35:56 +0100

    js03

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Introduction
+let intro = 'Introduction to Variables';
+console.log(intro);
+
+// Strings 
+const nameInput = 'deji';
+const backTick = `hello there ${nameInput} No ${2+2}! `;
+const doubleQuotes = "hello world!";
+const singleQuotes = 'hello kids!';
+
+console.log(backTick);
+console.log(doubleQuotes);
+console.log(singleQuotes);
+
+
+// Numbers 
+const wholeNumber = 5;
+const decimalNumber = 0.5;
+
+console.log(wholeNumber);
+console.log(decimalNumber);
+
+
+let firstNumber = 8;
+let secondNumber = 16;
+let result = firstNumber + secondNumber;
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(result);
